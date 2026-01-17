d3e8cb335adefdfe2d512e6f0495f5cec3f39859
Ahmed <hassainia.a@hotmail.fr>
2026-01-17 18:19:18 +0100

    feat(lists): creat routes skeleton

---
+++ routes/lists.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { authenticateToken } = require('../middleware/authMiddleWare');
+const listController = require('../controllers/listController');
+
+/* GET /api/lists - Get all lists for the authenticated user */
+
+/* Post /api/lists - Create a new list */
+
+/* GET /api/lists/:id - Get a specific list by ID */
+
+/* Put /api/lists/:id - Update a specific list by ID */
+
+/* DELETE /api/lists/:id - Delete a specific list by ID */
+
+module.exports = router;
